85e10bccfa0c041e6dffbccce99534fc01629f1e
harsh <harshkumaragarwal2003@gmail.com>
2024-03-07 01:21:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const cors = require('cors')
+const fs = require('fs-extra')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
+
+
+app.post('/logs', (req, res) => {
+    try {
+        const newjsonData = JSON.stringify(req.body,'',2);
+        const data = fs.readFileSync('data.json');
+        const jsonData = JSON.parse(data);
+        console.log(jsonData)
+        jsonData.data.push(newjsonData);
+        fs.writeFileSync('data.json', JSON.stringify(jsonData));
+        res.sendStatus(200)
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
+
+})
